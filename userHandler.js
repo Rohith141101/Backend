eacdce6ce8512c965f37eae6cef26d48d2a4159d
Rohith <73777100+Rohith141101@users.noreply.github.com>
2023-12-16 21:39:14 +0530

    Update userHandler.js

--- userHandler.js
+++ userHandler.js
@@ -1,7 +1,9 @@
 const userHandler=(err,req,res,next)=>{
     const statusCode=res.statusCode?res.statusCode:500
     res.status(statusCode)
-    res.send(err.message)
+    res.json({
+        message:err.message}
+        )
 
 }
 module.exports=userHandler;
