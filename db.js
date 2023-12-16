24acba4a7f89fabce138fe3a46b58e38dbc3154a
Rohith <73777100+Rohith141101@users.noreply.github.com>
2023-12-16 21:37:26 +0530

    Update db.js

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const connectDB=async()=>{
     try{
 
         const conn=await mongoose.connect(process.env.MONGO_URI)
-        console.log(`connected ${conn.connection}`)
+        console.log(`connected ${conn.connection.host}`)
     }
     catch(err){
         console.log(err.message)
