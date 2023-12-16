3b8d04ed50ca3cf1ec3995a09a97bb45c88cc029
Rohith <73777100+Rohith141101@users.noreply.github.com>
2023-12-16 21:39:57 +0530

    Update userRoute.js

--- userRoute.js
+++ userRoute.js
@@ -1,7 +1,12 @@
 const express=require('express')
-const { loginRouter, registerRouter } = require('./userController')
+const { loginRouter, registerRouter,getRouter,deleteRouter,updateRouter, getRouters } = require('./userController')
 const router=express.Router()
 
 router.post('/login',loginRouter)
+router.get('/',getRouter)
+router.get('/:id',getRouters)
+router.put('/:id',updateRouter)
+router.delete('/:id',deleteRouter)
 router.post('/',registerRouter)
+
 module.exports=router
