8db1af4a92c97008eadf994fe3edda3ebf9cb827
Rohith <73777100+Rohith141101@users.noreply.github.com>
2023-12-16 21:38:39 +0530

    Update userController.js

--- userController.js
+++ userController.js
@@ -35,7 +35,82 @@ const registerRouter=expressAsyncHandler(async(req,res)=>{
     }
     
 })
+const getRouter=expressAsyncHandler(async(req,res)=>{
+    // const {id}=req.params
+    const users= await User.find()
+    console.log(users)
+    if(users){
+        res.status(201)
+        res.json({
+            users
+        }
+    )
+    }
+    else{
+        res.status(400)
+        throw new Error("User not found")
+    }
+})
+
+const getRouters=expressAsyncHandler(async(req,res)=>{
+    const {id}=req.params
+    const user= await User.findById(id)
+    if(user){
+        res.status(200)
+        res.json({
+            user
+        }
+    )
+    }
+    else{
+        res.status(400)
+        throw new Error("User not found")
+    }
+})
+const updateRouter = expressAsyncHandler(async (req, res) => {
+    const { id } = req.params;
+      const user = await User.findById(id);
+  
+      if (user) {
+      const updatedUser = await User.findByIdAndUpdate(
+        id,
+        {
+          $set: req.body,
+        },
+        {
+          new: true,
+        }
+      );
+  
+      res.status(200).json({
+        updatedUser,
+      })
+    }
+    else{
+        res.status(404);
+        throw new Error("User not found");
+    }
+  })
+  
+
+const deleteRouter=expressAsyncHandler(async(req,res)=>{
+    const {id}=req.params
+    const users= await User.findById(id)
+    if(users){
+         User.findByIdAndDelete(id)
+        res.status(201)
+        res.json({
+            message:"user deleted successfully"
+        }
+    )
+    }
+    else{
+        res.status(400)
+        throw new Error("User not found")
+    }
+})
+
 const loginRouter=expressAsyncHandler(async(req,res)=>{
     res.send("login Route")
 })
-module.exports={registerRouter,loginRouter}
+module.exports={registerRouter,loginRouter,getRouter,updateRouter,deleteRouter,getRouters}
