7fb9cb6ae7734ba77665670f4597f36736e54c74
Rohith <73777100+Rohith141101@users.noreply.github.com>
2023-12-20 20:18:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express=require('express')
+var cors = require('cors')
 const userHandler = require('./userHandler')
 const connectDB = require('./db')
 const dotenv=require('dotenv').config()
 const PORT=process.env.PORT
 const app=express()
 connectDB()
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(userHandler)
